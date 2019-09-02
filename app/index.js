2a849dfc091d892a3675101b3f267da8b4bd0e3d
robiwankenobi <rfnkurle@gmail.com>
2019-09-02 15:24:35 -0700

    Pushing Github Battle

---
+++ app/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import Popular from './components/Popular'
+import Battle from './components/Battle'
+
+// components manages
+// State
+// Lifecycle
+// UI
+class App extends React.Component {
+
+render(){
+
+    return (
+    
+    <div className="container">
+        <Battle/>
+    </div>
+    )
+}
+
+
+}
+
+ReactDOM.render(
+    //2 arguments
+    // 1.React Element
+    <App />, 
+    //2. Where to render the element to
+    document.getElementById('app')
+    )
