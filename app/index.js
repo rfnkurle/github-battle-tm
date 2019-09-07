10ad308ff8750a49aab65bb971de553ec47c3c4a
robiwankenobi <rfnkurle@gmail.com>
2019-09-07 15:59:20 -0700

    Added ThemeConsumers to Battle and Card

--- app/index.js
+++ app/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
-import ThemeProvider from './contexts/theme'
+import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
-      theme: "light",
+      theme: 'light',
       toggleTheme: () => {
-        this.setState(({theme})=>({
+        this.setState(({ theme }) => ({
           theme: theme === 'light' ? 'dark' : 'light'
         }))
       }
@@ -22,12 +22,13 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider value={this.state}>
-      <div className={this.state.theme}>
-      <div className='container'>
-        <Nav />
-        <Battle />
-      </div>
-      </div>
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+
+            <Popular />
+          </div>
+        </div>
       </ThemeProvider>
     )
   }
